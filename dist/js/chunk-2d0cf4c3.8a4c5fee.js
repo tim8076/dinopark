4ddee218b16157685e7ef27a5ca7787ee983a921
shosetim <shosetim32@gmail.com>
2021-08-21 22:14:39 +0800

    完成第二次作業調整

--- dist/js/chunk-2d0cf4c3.8a4c5fee.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cf4c3"],{"62b3":function(e,c,t){"use strict";t.r(c);var o=t("7a23"),r={class:"cart-page"},a=Object(o["createVNode"])("div",{class:"title bg-cover"},[Object(o["createVNode"])("h2",{class:"fs-1 text-white"},"結帳流程")],-1);function n(e,c){var t=Object(o["resolveComponent"])("router-view");return Object(o["openBlock"])(),Object(o["createBlock"])("div",r,[a,Object(o["createVNode"])(t)])}const s={};s.render=n;c["default"]=s}}]);
-//# sourceMappingURL=chunk-2d0cf4c3.8a4c5fee.js.map
